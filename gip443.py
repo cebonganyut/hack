25cf4ba210f1e12d0c386149866641d04b518ed9
cebonganyut <89703592+cebonganyut@users.noreply.github.com>
2024-09-16 08:36:32 +0000

    Update gip3.py

--- gip443.py
+++ gip443.py
@@ -1,43 +1,68 @@
-import socket, requests, re, sys, threading
+import socket
+import requests
+import re
+import sys
+import threading
 from queue import Queue
 from colorama import Fore, init
 from itertools import cycle
 from multiprocessing.dummy import Pool as ThreadPool
+
+init()  # Inisialisasi colorama
+
 white = Fore.LIGHTWHITE_EX
 red = Fore.LIGHTRED_EX
 green = Fore.LIGHTGREEN_EX
 yellow = Fore.LIGHTYELLOW_EX
 cyan = Fore.CYAN
 
-def taz(ip):
+def check_port(ip, port):
     try:
-        ip = ip.replace('\n', '').replace('\r', '')
+        ip = ip.strip()
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         sock.settimeout(5)
-        result = sock.connect_ex((ip, 443))
-        if str(result) != '0':
-            print(red + '[xx] Bad Ip >>> ' + ip + white)
-            open('Result/Badip.txt', 'a').write(ip + '\n')
-        if str(result) == '0':
-            print(cyan + '[>>] Good Ip >>> ' + ip + white)
-            open('Result/Goodip443.txt', 'a').write(ip + '\n')
+        result = sock.connect_ex((ip, port))
+        sock.close()
+        return result == 0
     except:
-        pass
+        return False
 
+def taz(ip):
+    ip = ip.strip()
+    port_80_open = check_port(ip, 80)
+    port_443_open = check_port(ip, 443)
 
-banner =  '''
+    if port_80_open or port_443_open:
+        if port_80_open and port_443_open:
+            print(green + f'[>>] Good IP (80 & 443 open) >>> {ip}' + white)
+        elif port_80_open:
+            print(cyan + f'[>>] Good IP (80 open) >>> {ip}' + white)
+        else:
+            print(cyan + f'[>>] Good IP (443 open) >>> {ip}' + white)
+        open('Result/Goodip_80_443.txt', 'a').write(ip + '\n')
+    else:
+        print(red + f'[xx] Bad IP >>> {ip}' + white)
+        open('Result/Badip.txt', 'a').write(ip + '\n')
+
+banner = '''
 Tools by insan1337 with chat gpt
-A Private Tool To Check GoodIPS
-
-
+A Private Tool To Check GoodIPS for Port 80 and 443
 '''
 
 print(banner)
-David = open(input(''  ' list :  ' ''), 'r').readlines()
-Davidd = input(''  ' thread : '  '')
-pool = ThreadPool(int(Davidd))
-pool.map(taz, David)
+
+ip_list_file = input('Enter IP list file: ')
+thread_count = input('Enter thread count: ')
+
+with open(ip_list_file, 'r') as f:
+    ip_list = f.readlines()
+
+pool = ThreadPool(int(thread_count))
+pool.map(taz, ip_list)
 pool.close()
 pool.join()
+
 if __name__ == '__main__':
-    print('Done !! Check /Result/Goodip.txt !!')
+    print('Done! Check Result folder for output files:')
+    print('- Goodip_80_443.txt (IPs with either 80 or 443 or both open)')
+    print('- Badip.txt (IPs with neither 80 nor 443 open)')
